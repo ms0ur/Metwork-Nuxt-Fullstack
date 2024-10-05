a24194824339ae095fe99eb8984f365b7dbd0a07
ms0ur <maksimsuslov7142@gmail.com>
2024-10-05 11:41:22 +0300

    started making a user page

---
+++ pages/user/[identifier].vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const route = useRoute();
+const identifier = route.params.identifier;
+
+const { data, status, error } = await useFetch(`/api/user/${identifier}/getUser`);
+
+if (status.value == "success") {
+  console.log(data.value);
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
